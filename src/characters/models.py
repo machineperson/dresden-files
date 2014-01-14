21b79b88ae87c4da735ba71b4689999a247dba6f
Nadja Deininger <nadja@ef.gy>
2014-01-14 04:46:56 +0000

    intermediate commit, trying to get signals to work

--- src/characters/models.py
+++ src/characters/models.py
@@ -1,14 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
 
-
-class Phase(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    phase_aspect = models.CharField('Phase Aspect', max_length=300)
-    guest_star = models.CharField(max_length=200, blank=True)
-    
-    def __unicode__(self):
-        return self.name + ": " + self.phase_aspect
 
 
 class Stunt(models.Model):
@@ -38,14 +30,22 @@ class Character(models.Model):
     base_refresh = models.IntegerField('Base refresh')
     notes = models.TextField(blank=True)
     inventory = models.TextField(blank=True)
-    phases = models.ManyToManyField(Phase) 
     stunts = models.ManyToManyField(Stunt, blank='True')
     powers = models.ManyToManyField(Power, blank='True')
-    skills = models.ManyToManyField(Skill, through='CharacterSkill')
+    skills = models.ManyToManyField(Skill, through='CharacterSkill', blank='True')
     admin_hash = models.CharField(max_length=200)
     
     def __unicode__(self):
         return self.name
+
+class Phase(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    phase_aspect = models.CharField('Phase Aspect', max_length=300)
+    guest_star = models.CharField(max_length=200, blank=True)
+    character = models.ForeignKey(Character)
+    def __unicode__(self):
+        return self.name + ": " + self.phase_aspect
 
 class CharacterSkill(models.Model):
     skill = models.ForeignKey(Skill)
@@ -54,3 +54,9 @@ class CharacterSkill(models.Model):
 
     class Meta:
         ordering = ["rating"]
+
+def ensure_phases(sender, **kwargs):
+    if kwargs.get('created', False):
+        Phase.objects.create(character=kwargs.get('instance'), name='Background', phase_aspect=' ')
+
+post_save.connect(ensure_phases, sender=Character)
